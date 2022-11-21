a0d8288d1b79bd212a3aea40b62aabf980e2753a
Matt Howell <matthew.howell07@gmail.com>
2022-11-21 00:15:47 -0600

    generate qr code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
 import { useState } from 'react'
 
-// custom components
-import CustomForm from './components/CustomForm'
+// libraries
+import QRCode  from 'qrcode'
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [qrcode, setQrcode] = useState('');
+
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, (err, url) => {
+      if (err) return console.error(err)
+
+      console.log(url)
+      setQrcode(url)
+    })
+  }
 
   return (
     <div className="container">
       <header>
         <h1>QR Code Generator</h1>
       </header>
-      <CustomForm />
+      <div className="wrapper">
+        <input
+          type="text"
+          id="url"
+          className="input"
+          value={url}
+          onInput={(e) => setUrl(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Url"
+        />
+      </div>
+      <button
+        className="btn"
+        aria-label="Add Url"
+        type="submit"
+        onClick={GenerateQRCode}
+        >
+        Generate
+      </button>
+      <img src={qrcode} />
     </div>
   )
 }
